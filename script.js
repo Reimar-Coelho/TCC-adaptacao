2224a07f86326f9d19cf97fa9e83b7f3a37e4be3
Reimar-Coelho <reimargamer@proton.me>
2023-10-03 13:07:30 -0300

    mudanças drásticas e arrumando o código

--- script.js
+++ script.js
@@ -1,30 +1,106 @@
-const dropdowns = document.querySelectorAll('.dropdown');
+const dropdowns = document.querySelectorAll(".dropdown");
 
-dropdowns.forEach(dropdown => {
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelectorAll('menu li');
-    const selected = dropdown.querySelector('.selected');
-    select.addEventListener('click', () => {
-        select.classList.toggle('select-clicked');
-        caret.classList.toggle('caret-rotate');
-        menu.classList.toggle('menu-open');
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll("menu li");
+  const selected = dropdown.querySelector(".selected");
+  select.addEventListener("click", () => {
+    select.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
     });
-    options.forEach(option => {
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove('select-clicked');
-            caret.classList.remove('caret-rotate');
-            menu.classList.remove('menu-open');
-            options.forEach(option => {
-                option.classList.remove('active');
-            });
-            option.classList.add('active');
-        });
-    });
+  });
 });
 
 function erro() {
-    window.location.href = "../erro404/index.html";
+  window.location.href = "../erro404/index.html";
 }
+
+particlesJS("particles", {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 700,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+    },
+    opacity: {
+      value: 0.8,
+      random: true,
+      animation: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+    },
+  },
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onHover: {
+        enable: true,
+        mode: "push",
+      },
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+    },
+  },
+  retina_detect: true,
+});
